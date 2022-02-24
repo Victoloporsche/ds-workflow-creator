d9527c60b02bd5ae1ec99368e4b48d2e599181a6
Victor Uwaje <v.uwaje@enviosystems.com>
2022-02-24 18:22:14 +0100

    add ds workflow creator

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ds-workflow',
+    version='0.0.1',
+    description='Data Science end-to-end Workflow',
+    author='Victor Uwaje',
+    packages=find_packages(),
+    license='MIT'
+)
