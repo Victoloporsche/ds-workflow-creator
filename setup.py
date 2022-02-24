c5366abf5ca9a96b7829bbb9ee4c55596e7d64ba
Victor Uwaje <v.uwaje@enviosystems.com>
2022-02-24 19:55:05 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ds-workflow',
-    version='0.0.3',
+    version='0.0.30',
     description='Data Science end-to-end Workflow',
     author='Victor Uwaje',
     packages=find_packages(),
